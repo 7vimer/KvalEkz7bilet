3261592372c390f3aa660a054f99ad89983a473e
316de <316de@315-DE-8>
2023-07-05 09:43:14 +0300

    Добавьте файлы проекта.

---
+++ KvalEkz7bilet/Driver.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.OleDb;
+using System.Reflection.Emit;
+using System.Text;
+
+namespace KvalEkz7bilet
+{
+    public class Driver
+    {
+        //Поминов Даниил
+        public string Name { get; set; }
+        public string Class { get; set; }
+        public List<Auto> Autos = new List<Auto>();
+        public Driver(string name, string aClass) 
+        { 
+            Name = name;
+            Class = aClass;
+        }
+        public string ShowAll()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.Append($"Информация о водителе: \nИмя - {Name}\nКвалификация - {Class}\n\nИнформация об его автомобилях:\n");
+            foreach (Auto auto in Autos)
+            {
+                sb.Append(auto.Show());
+            }
+            return sb.ToString();
+            //Поминов Даниил
+        }
+    }
+
+    public class Auto : IComparable
+    {
+        public string Title { get; set; }
+        public string Number { get; set; }
+        public Auto(string title, string number)
+        {
+            Title = title;
+            Number = number;
+        }
+        public string Show()
+        {
+            return $"Авто - {Title} {Number}\n";
+        }
+        public int CompareTo(object obj)
+        {
+            return (this.Title + this.Number).CompareTo(Title + Number);
+        }
+    }
+    //Поминов Даниил
+}
---
+++ KvalEkz7biletConsole/Program.cs
@@ -0,0 +1,26 @@
+﻿using KvalEkz7bilet;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KvalEkz7biletConsole
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Driver driver = new Driver("Поминов Даниил", "A, B, M");
+            Auto auto1 = new Auto("Suzuki Grand Vitara", "К124ЕХ777");
+            Auto auto2 = new Auto("Lexus IS 200", "М753ТР97");
+            Auto auto3 = new Auto("VW Transporter t6", "А142ПА777");
+            driver.Autos.Add(auto1);
+            driver.Autos.Add(auto2);
+            driver.Autos.Add(auto3);
+            Console.WriteLine(driver.ShowAll());
+            Console.ReadLine();
+            //Поминов Даниил
+        }
+    }
+}
